28bd06f7ffff2804e667bad07e3120fe36bd8417
Kaleeswaran <kaleeswaran@iinerds.com>
2020-06-08 21:33:33 +0530

    sidenav added

--- designs-app/src/app/app.component.ts
+++ designs-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'designs-app';
+  title = "designs-app";
+  sidenavList = [
+    { path: "../assets/fb.svg", name: "Facebook" },
+    { path: "../assets/instagram.svg", name: "Instagram" },
+    { path: "../assets/placeholder.svg", name: "Locations" },
+  ];
 }
